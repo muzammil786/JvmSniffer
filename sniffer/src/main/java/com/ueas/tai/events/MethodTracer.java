5dad1f46b53a62a8bd2ad092b9740f8e81940db2
Muzammil Shahbaz <muzammil.shahbaz@gmail.com>
2019-04-17 12:30:15 +0100

    changed method info collection from set to ordered set to keep the order of method calls

--- sniffer/src/main/java/com/ueas/tai/events/MethodTracer.java
+++ sniffer/src/main/java/com/ueas/tai/events/MethodTracer.java
@@ -18,18 +18,20 @@ import org.jdiscript.handlers.OnMethodEntry;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class MethodTracer implements OnMethodEntry {
 
   private static final Logger LOGGER = LogManager.getLogger(MethodTracer.class);
-  
-  Set<MethodInfo> methodInfoSet = new HashSet<>();
+  // ordered set of method calls
+  Set<MethodInfo> methodInfoSet = new LinkedHashSet<>();
 
   @Override
   public void methodEntry(MethodEntryEvent event) {
     Method method = event.method();
     MethodInfo methodInfo = new MethodInfo(method);
+    LOGGER.debug("Call intercepted for " + methodInfo.toString());
     methodInfoSet.add(methodInfo);
   }
 
